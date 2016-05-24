fea5d9477960968d5f7690412d881459435b1090
Dao Hoang Son <daohoangson@gmail.com>
2016-05-24 14:08:40 +0700

    added proxy config via config.gradle

--- app/src/main/java/com/daohoangson/pubvncast/networking/CorsProxy.java
+++ app/src/main/java/com/daohoangson/pubvncast/networking/CorsProxy.java
@@ -1,10 +1,18 @@
 package com.daohoangson.pubvncast.networking;
 
+import android.text.TextUtils;
+
+import com.daohoangson.pubvncast.BuildConfig;
+
 import java.io.UnsupportedEncodingException;
 
 public class CorsProxy {
+
     public static String buildUrl(String url) throws UnsupportedEncodingException {
-        return String.format("http://125.212.247.216:1337/%s",
-                url.replaceAll("^http://", ""));
+        if (TextUtils.isEmpty(BuildConfig.CORS_PROXY_URL)) {
+            return url;
+        }
+
+        return String.format("%s/%s", BuildConfig.CORS_PROXY_URL, url.replaceAll("^http://", ""));
     }
 }
